8c08732aa0e4cd32d1b4cc4f10cc679e4820c17d
Karan Gupta <d22124440@mytudublin.ie>
2023-06-08 20:12:57 +0100

    time, author, reactions added to post

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
 
 export const store = configureStore({
   reducer: {
     // will hold the reducers we will create
     posts: postsReducer,
+    users: usersReducer,
   },
 });
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,28 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post) => (
+  // to show the latest post first and oldest one in the end
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
+      {/* Post */}
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
+
+      {/* post Author */}
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        {/* Time Ago */}
+        <TimeAgo timestamp={post.date} />
+      </p>
     </article>
   ));
 
